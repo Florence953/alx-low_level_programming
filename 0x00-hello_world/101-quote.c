ddf5eb0a16da04badaa62446a9976ab2ec924a7d
Florence953 <florencemuia953@gmail.com>
2022-10-18 02:38:50 -0700

    error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - outputs the statement with standard error
+ *
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+
+	return (1);
+}
